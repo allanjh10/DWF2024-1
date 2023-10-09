2a497b5f392033c0bdcb4514bc5b55af4d516f0a
cortezgretel <gretel141000@ciencias.unam.mx>
2023-10-08 19:22:57 -0600

    Practica 7: modal y boton para subir una nueva categoria

--- src/app/modules/product/components/category/category.component.ts
+++ src/app/modules/product/components/category/category.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Category } from '../../_models/category';
 import { FormBuilder, Validators } from '@angular/forms';
 
+declare var $: any; // Declara la variable $ de JQuery para utilizarla en el componente
+
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
@@ -9,6 +11,16 @@ import { FormBuilder, Validators } from '@angular/forms';
 })
 export class CategoryComponent{
   categories: Category[] = [];
+
+  // Crea un objeto FormGroup que contiene los objetos FormControl
+  form = this.formBuilder.group({
+    category: ["", [Validators.required]],
+    code: ["", [Validators.required]],
+  });
+
+  // Variable para saber si algo fue enviado
+  submitted = false;
+
 
   constructor(private formBuilder: FormBuilder,){ }
 
@@ -23,5 +35,28 @@ export class CategoryComponent{
     const category3 = new Category(3, '083JD35', 'JORDAN', 1);
 
     this.categories.push(category1, category2, category3);
+  }
+
+  onSubmit(){
+    this.submitted = true;
+
+    if(this.form.invalid) return;
+
+    this.submitted = false;
+
+    let category = new Category(0,this.form.controls['code'].value!, this.form.controls['category'].value!, 1);
+    this.categories.push(category);
+    
+    $("#modalForm").modal("hide");
+
+    alert("¡La Categoría ha sido guardada con exito!");
+  }
+
+  // modals 
+
+  showModalForm(){
+    this.form.reset();
+    this.submitted = false;
+    $("#modalForm").modal("show");
   }
 }
